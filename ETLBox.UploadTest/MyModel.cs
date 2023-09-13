8960dad1a4761b995a5975d527b15396a7566621
Maurice Brand-Freitag <freitag@fastec.de>
2023-09-13 15:51:44 +0200

    added proj

---
+++ ETLBox.UploadTest/MyModel.cs
@@ -0,0 +1,52 @@
+﻿using ETLBox.DataFlow;
+
+namespace ETLBox.UploadTest
+{
+    public class MyModel
+    {
+        [IdColumn]
+        [DistinctColumn]
+        public Guid Id { get; set; }
+        public string SomeString { get; set; } = "";
+        public DateTime Starttime { get; set; }
+        public DateTime Endtime { get; set; }
+        public int SomeInt1 { get; set; }
+        public int SomeInt2 { get; set; }
+        public Guid SomeId1 { get; set; }
+        public Guid SomeId2 { get; set; }
+        public Guid SomeId3 { get; set; }
+        public double SomeDouble1 { get; set; }
+        public double SomeDouble2 { get; set; }
+        public double SomeDouble3 { get; set; }
+        public double SomeDouble4 { get; set; }
+        public double SomeDouble5 { get; set; }
+        public double SomeDouble6 { get; set; }
+        public double SomeDouble7 { get; set; }
+        public double SomeDouble8 { get; set; }
+
+        public static string TableName { get; } = "mymodel";
+        public static string Sql { get; } =
+            $"""
+                SELECT 
+                    x.Id,
+                    x.SomeString,
+                    x.Starttime,
+                    x.Endtime,
+                    x.SomeInt1,
+                    x.SomeInt2,
+                    x.SomeId1,
+                    x.SomeId2,
+                    x.SomeId3,
+                    x.SomeDouble1,
+                    x.SomeDouble2,
+                    x.SomeDouble3,
+                    x.SomeDouble4,
+                    x.SomeDouble5,
+                    x.SomeDouble6,
+                    x.SomeDouble7,
+                    x.SomeDouble8
+                FROM 
+                    {TableName} AS x
+            """;
+    }
+}
---
+++ ETLBox.UploadTest/Program.cs
@@ -0,0 +1,151 @@
+﻿using ETLBox.ControlFlow;
+using ETLBox.DataFlow;
+using ETLBox.Parquet;
+using ETLBox.SqlServer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ETLBox.UploadTest
+{
+    internal static class Program
+    {
+        private static readonly bool resetData = false;
+        private static readonly int dataEntries = 5_000;
+        private static readonly Random r = new();
+
+        private static readonly string sqlServerConnectionString = "";
+        private static readonly string storageAccountConnectionString = "";
+        private static readonly string containerName = "";
+
+        public static async Task Main()
+        {
+            Settings.LogInstance = GetLogger();
+            await PrepareDataAsync().ConfigureAwait(false);
+            await ExportDataAsync().ConfigureAwait(false);
+        }
+
+        private static ILogger GetLogger()
+        {
+            var services = new ServiceCollection();
+            services.AddLogging(x => x.AddConsole());
+            return services.BuildServiceProvider().GetRequiredService<ILogger<Network>>();
+        }
+
+        private static async Task ExportDataAsync()
+        {
+            var network = CreateNetwork();
+            await network.ExecuteAsync().ConfigureAwait(false);
+        }
+
+        private static Network CreateNetwork()
+        {
+            using var connectionManager = new SqlConnectionManager(sqlServerConnectionString);
+            var source = new DbSource<MyModel>(connectionManager) { Sql = MyModel.Sql };
+
+            var destination = new ParquetDestination<MyModel>(GetFileName(0), ResourceType.AzureBlob);
+            destination.AzureBlobStorage.ConnectionString = storageAccountConnectionString;
+            destination.AzureBlobStorage.ContainerName = containerName;
+            destination.AzureBlobStorage.BlockBlobOpenWriteOptions = new()
+            {
+                Tags = new Dictionary<string, string>
+                {
+                    { "state", "unprocessed" },
+                    { "tenantid", $"{Guid.NewGuid()}" },
+                }
+            };
+            source.LinkTo(destination);
+            return new Network(source);
+        }
+
+        private static string GetFileName(int progress)
+        {
+            var fileNameTemplate = "MyModel_d{Date}_{Progress}.parquet";
+            var fileName = fileNameTemplate.Replace("{Date}", DateTime.UtcNow.ToString("yyyyMMddHHmmss"));
+            fileName = fileName.Replace("{Progress}", progress.ToString());
+            return fileName;
+        }
+
+        private static async Task PrepareDataAsync()
+        {
+            Console.WriteLine("Preparing data.");
+            using var connectionManager = new SqlConnectionManager(sqlServerConnectionString);
+            var tables = GetTableListTask.ListAll(connectionManager);
+            var tableExists = tables.Exists(x => x.UnquotatedObjectName == MyModel.TableName);
+
+            if (!tableExists)
+            {
+                Console.WriteLine($"Ensuring table {MyModel.TableName} exists.");
+                CreateTableTask.CreateIfNotExists(connectionManager, MyModel.TableName,
+                    typeof(MyModel).GetProperties().Select(x => new ControlFlow.TableColumn(
+                        name: x.Name,
+                        dataType: GetDataType(x.PropertyType),
+                        allowNulls: false,
+                        isPrimaryKey: x.Name == nameof(MyModel.Id)
+                        )).ToList());
+            }
+            if (!tableExists || resetData)
+            {
+                Console.WriteLine($"Deleting everything from {MyModel.TableName}.");
+                SqlTask.ExecuteNonQuery(connectionManager,
+                    $"DELETE FROM {MyModel.TableName}");
+
+                var remainingEntries = dataEntries;
+                var batchSize = 100_000;
+                while (remainingEntries > 0)
+                {
+                    Console.WriteLine($"{remainingEntries} entries remaining.");
+                    await PrepareDataBatchAsync(Math.Min(remainingEntries, batchSize), connectionManager).ConfigureAwait(false);
+                    remainingEntries -= batchSize;
+                }
+            }
+        }
+
+        private static string GetDataType(Type propertyType)
+        {
+            if (propertyType == typeof(string))
+                return "NVARCHAR(128)";
+            if (propertyType == typeof(Guid))
+                return "UNIQUEIDENTIFIER";
+            if (propertyType == typeof(int))
+                return "INT";
+            if (propertyType == typeof(double))
+                return "DECIMAL(28,8)";
+            if (propertyType == typeof(DateTime))
+                return "DATETIME";
+            throw new NotSupportedException();
+        }
+
+        private static async Task PrepareDataBatchAsync(int batchSize, SqlConnectionManager connectionManager)
+        {
+            var source = new MemorySource<MyModel>
+            {
+                Data = Enumerable.Range(0, batchSize).Select(_ => GetModel())
+            };
+            var destination = new DbDestination<MyModel>(connectionManager, MyModel.TableName);
+            source.LinkTo(destination);
+            var network = new Network(source);
+            await network.ExecuteAsync().ConfigureAwait(false);
+        }
+
+        private static MyModel GetModel() => new()
+        {
+            Id = Guid.NewGuid(),
+            SomeInt1 = r.Next(),
+            SomeInt2 = r.Next(),
+            SomeId1 = Guid.NewGuid(),
+            SomeId2 = Guid.NewGuid(),
+            SomeId3 = Guid.NewGuid(),
+            Starttime = DateTime.Now.AddSeconds(r.NextDouble()),
+            Endtime = DateTime.Now.AddSeconds(r.NextDouble()),
+            SomeDouble1 = r.NextDouble(),
+            SomeDouble2 = r.NextDouble(),
+            SomeDouble3 = r.NextDouble(),
+            SomeDouble4 = r.NextDouble(),
+            SomeDouble5 = r.NextDouble(),
+            SomeDouble6 = r.NextDouble(),
+            SomeDouble7 = r.NextDouble(),
+            SomeDouble8 = r.NextDouble(),
+            SomeString = $"{Guid.NewGuid()}{Guid.NewGuid()}"
+        };
+    }
+}
